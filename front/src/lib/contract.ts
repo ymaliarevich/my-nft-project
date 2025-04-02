9b75feab433e310ec104ab004a48f54e39995899
ymaliarevich <ymaliarevich@gmail.com>
2025-04-02 12:18:55 +0200

    minor fix

--- front/src/lib/contract.ts
+++ front/src/lib/contract.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers'
 
 const CONTRACT_ADDRESS = '0xE5A1AD65436D9Fd28B106ca0ed88BF06D22B4380'
-import MyNFTJson from '../../../blockchain/artifacts/contracts/MyNFT.sol/MyNFT.json'
+import MyNFTJson from '../lib/abi/MyNFT.json'
 
 export function getContract(providerOrSigner: ethers.Provider | ethers.Signer) {
     return new ethers.Contract(CONTRACT_ADDRESS, MyNFTJson.abi, providerOrSigner)
